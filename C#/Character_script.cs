de6d67be93b6c65692a8deb8a38525a32f12b1b6
Léo Cazenave <leo@cazenave.cc>
2021-02-17 19:07:59 +0100

    Added the ability to walk instead of run.

--- C#/Character_script.cs
+++ C#/Character_script.cs
@@ -7,7 +7,10 @@ public class Character_script : MonoBehaviour {
 	private Rigidbody2D rb;  // rb is used to enable and control the physics of the character.
 
 	public float runSpeed = 8.5f;  // Movement speed when running (not the initial dash)
+	public float walkSpeed = 3.5f;  // Movement speed when walking.
 	private float horizontalMove = 0f;	 // How far analog stick is pressed on horizontal axis (between -1 and +1).
+	private bool smashMode = false;  // Determines if the normal attack button will do smashs or tilt attacks,
+									 // and if theplayer will walk or run.
 
 	// Use this for initialization
 	void Awake () {
@@ -17,6 +20,18 @@ public class Character_script : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		horizontalMove = Input.GetAxisRaw("Horizontal");
-		rb.velocity = new Vector2(horizontalMove * runSpeed, rb.velocity.y);
+		if (Input.GetKeyDown(KeyCode.A)) {
+			smashMode = true;
+		}
+		if (Input.GetKeyUp(KeyCode.A)) {
+			smashMode = false;
+		}
+		if (smashMode) {
+			rb.velocity = new Vector2(horizontalMove * runSpeed, rb.velocity.y);
+
+		}
+		else {
+			rb.velocity = new Vector2(horizontalMove * walkSpeed, rb.velocity.y);
+		}
 	}
 }
