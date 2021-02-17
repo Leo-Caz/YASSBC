4373c7870bf45ad30716de885571c8890c7c030f
Léo Cazenave <leo@cazenave.cc>
2021-02-17 16:40:48 +0100

    Added Dummy left/right movement to character.

---
+++ C#/Character_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_script : MonoBehaviour {
+
+	private Rigidbody2D rb;  // rb is used to enable and control the physics of the character.
+
+	public float runSpeed = 0f;  // Runspeed is set to 0 in the script, but will be set in the Editor.
+	private float horizontalMove = 0f;	 // How far analog stick is pressed on horizontal axis (between -1 and +1).
+
+	// Use this for initialization
+	void Awake () {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontalMove = Input.GetAxisRaw("Horizontal");
+		rb.velocity = new Vector2(horizontalMove * runSpeed, rb.velocity.y);
+	}
+}
