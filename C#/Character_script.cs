c4cd7a2727bdce2ab0d76fda31b938ddcf10bbc9
Léo Cazenave <leo@cazenave.cc>
2021-02-19 11:48:47 +0100

    Added different types of jumps.

--- C#/Character_script.cs
+++ C#/Character_script.cs
@@ -6,20 +6,22 @@ public class Character_script : MonoBehaviour {
 
 	private Rigidbody2D rb;  // rb is used to enable and control the physics of the character.
 
-	public float dashSpeed = 16f;    // Speed of the initial dash.
-	public float runSpeed  = 9.5f;   // Movement speed when running (not the initial dash)
-	public float walkSpeed = 4.5f;   // Maximum movement speed when walking.
-	public float jumpHeight = 25f;   // Height of the peak of the jump.
-	public float maxFallSpeed = -8.5f;  // Maximum fall speed without fast falling.
-	public int maxNbJumps = 2;       // Number of times the player can jump before touching the ground.
-	public float airAccel = 14.5f;   // Acceleration when moving in the air.
-	public float airSpeed = 7.75f;   // Maximum speed in the air.
-	public bool isGrounded = false;  // Determines if the player is touching the ground.
+	public float dashSpeed = 16f;         // Speed of the initial dash.
+	public float runSpeed  = 9.5f;        // Movement speed when running (not the initial dash)
+	public float walkSpeed = 4.5f;        // Maximum movement speed when walking.
+	public float fullHopHeight = 25f;     // Height of the normal jump.
+	public float shortHopHeight = 18f;    // Height of the short jump.
+	public float doubleJumpHeight = 27f;  // Height of the double jump.
+	public int maxNbJumps = 2;            // Number of times the player can jump before touching the ground.
+	public float maxFallSpeed = -8.5f;    // Maximum fall speed without fast falling.
+	public float airAccel = 14.5f;        // Acceleration when moving in the air.
+	public float airSpeed = 7.75f;        // Maximum speed in the air.
+	public bool isGrounded = false;       // Determines if the player is touching the ground.
 
-	private float horizontalMove = 0f;	 // How far analog stick is pressed on horizontal axis (between -1 and +1).
-	private bool ableToDash = true;  // Determines if the player does the initial dash when running.
-	private bool smashMode = false;  // Determines if normal atk button will do smashs or tilts, and if player will walk or run.
-	private int jumpsUsed = 0;       // Number of jumps performed before touching the ground.
+	private float horizontalMove = 0f;	  // Position of the analog stick x-axis (between -1 and +1).
+	private bool ableToDash = true;       // Determines if the player does the initial dash when running.
+	private bool smashMode = false;       // Determines if normal atk button will do smashs or tilts, and if player will walk or run.
+	private int jumpsUsed = 0;            // Number of jumps performed before touching the ground.
 
 
 	// Use this for initialization
@@ -37,7 +39,11 @@ public class Character_script : MonoBehaviour {
 		else if (Input.GetKeyUp(KeyCode.A)) smashMode = false;
 
 		// Calls the Jump function when spacebar is pressed and you still have jumps left.
-		if ((Input.GetKeyDown(KeyCode.Space)) && (jumpsUsed < maxNbJumps)) Jump();
+		if ((Input.GetKeyDown(KeyCode.Space)) && (jumpsUsed < maxNbJumps)) {
+			if (isGrounded && !smashMode) Jump(0);      // Perform a standard jump.
+			else if (isGrounded && smashMode) Jump(1);  // Perform a short jump.
+			else if (!isGrounded) Jump(2);              // Perform a double jump.
+		}
 	}
 
 
@@ -76,10 +82,11 @@ public class Character_script : MonoBehaviour {
 	}
 
 
-	void Jump() {  // A standard Jump.
+	void Jump (int jumpType) {  // A standard Jump.
 		jumpsUsed++;
 		isGrounded = false;
 		rb.velocity = new Vector2(rb.velocity.x, 0f);
+		float jumpHeight = new float[] {fullHopHeight, shortHopHeight, doubleJumpHeight}[jumpType];
 		rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
 	}
 
