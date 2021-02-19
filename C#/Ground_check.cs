b4755a25f49a4d0c5d9ae4308bff2f2ffe5f8e32
Léo Cazenave <leo@cazenave.cc>
2021-02-19 15:35:44 +0100

    Fixed the indentation at certain lines.

--- C#/Ground_check.cs
+++ C#/Ground_check.cs
@@ -14,14 +14,14 @@ public class Ground_check : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D (Collider2D collider) {
-	    if ((collider.gameObject.CompareTag("Ground") || collider.gameObject.CompareTag("Plateform")) && rb.velocity.y < 0.1f) {
+		if ((collider.gameObject.CompareTag("Ground") || collider.gameObject.CompareTag("Plateform")) && rb.velocity.y < 0.1f) {
 			character_script.isGrounded = true;
-	    }
+		}
 	}
 
 	void OnTriggerExit2D (Collider2D collider) {
-	    if (collider.gameObject.CompareTag("Ground") || collider.gameObject.CompareTag("Plateform")) {
+		if (collider.gameObject.CompareTag("Ground") || collider.gameObject.CompareTag("Plateform")) {
 			character_script.isGrounded = false;
-	    }
+		}
 	}
 }
