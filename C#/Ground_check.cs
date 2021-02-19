b50253717692d3a3fca4d65a44f988c6ea8b103d
Léo Cazenave <leo@cazenave.cc>
2021-02-19 13:40:54 +0100

    Groung cannot be detected when going up now.

--- C#/Ground_check.cs
+++ C#/Ground_check.cs
@@ -6,13 +6,15 @@ public class Ground_check : MonoBehaviour {
 
 	public GameObject character;
 	private Character_script character_script;
+	private Rigidbody2D rb;
 
 	void Awake() {
 		character_script = character.GetComponent<Character_script>();
+		rb = character.GetComponent<Rigidbody2D>();
 	}
 
 	void OnTriggerEnter2D (Collider2D collider) {
-	    if (collider.gameObject.CompareTag("Ground")) {
+	    if (collider.gameObject.CompareTag("Ground") && rb.velocity.y < 0) {
 			character_script.isGrounded = true;
 	    }
 	}
