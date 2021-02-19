96890b3c3ae1482c7e028e3cafc0bc78c75f251c
Léo Cazenave <leo@cazenave.cc>
2021-02-19 09:31:25 +0100

    Added a Ground_check gameObject and script.

---
+++ C#/Ground_check.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground_check : MonoBehaviour {
+
+	public bool isGrounded = true;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+	    Debug.Log("Entered");
+	    if (collider.gameObject.CompareTag("Ground")) {
+	        isGrounded = true;
+	    }
+	}
+
+	void OnTriggerExit2D(Collider2D collider) {
+	    Debug.Log("Exited");
+	    if (collider.gameObject.CompareTag("Ground")) {
+	        isGrounded = false;
+	    }
+	}
+}
