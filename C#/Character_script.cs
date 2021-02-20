768fdb6b7f645c40998b4a385a042fea89a0d67a
Léo Cazenave <leo@cazenave.cc>
2021-02-20 11:40:44 +0100

    Added an air dodge mechanic.

--- C#/Character_script.cs
+++ C#/Character_script.cs
@@ -17,15 +17,23 @@ public class Character_script : MonoBehaviour {
 	public float fastFallSpeed = -14.5f;  // Speed of the player when performing a fast-fall.
 	public float airAccel = 14.5f;        // Acceleration when moving in the air.
 	public float airSpeed = 7.75f;        // Maximum speed in the air.
+	public float normalDrag = 2.6f;       // Value of the regular linear drag.
+	public float airDodgeDrag = 2.6f;     // Value of the linear drag during an air dodge.
+	public float airDodgeDist = 0f;       // Distance of the air dodge.
+	public float gravity = 0f;            // Gravity of the character.
 
 	public bool isGrounded = false;       // Determines if the player is touching the ground.
 	public bool insidePlateform = false;  // Determines if the player is inside a platform when going through.
+	public bool isFastFalling = false;    // Determines if the player performes a fast-fall or not.
 
 	private float horizontalMove = 0f;	  // Position of the analog stick x-axis (between -1 and +1).
+	private float verticalMove = 0f;	  // Position of the analog stick y-axis (between -1 and +1).
 	private bool ableToDash = true;       // Determines if the player does the initial dash when running.
-	private bool smashMode = false;       // Determines if normal atk button will do smashs or tilts, and if player will walk or run.
+	private bool isWalking = false;       // Determines if normal atk button will do smashs or tilts, and if player will walk or run.
 	private int jumpsUsed = 0;            // Number of jumps performed before touching the ground.
-	private bool isFastFalling = false;   // Determines if the player performes a fast-fall or not.
+	private bool isAirDodging = false;    // Determines if the player is in air dodge state.
+	private bool ableToAirDodge = true;   // Determines if the player is able to perform an air dodge.
+	private bool dirAirDodge = false;     // Determines if the player performed a directional AD or a normal one.
 
 
 	// Use this for initialization
@@ -37,19 +45,22 @@ public class Character_script : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		horizontalMove = Input.GetAxisRaw("Horizontal");  // Gets position of analog stick on x-axis
+		verticalMove = Input.GetAxisRaw("Vertical");  // Gets position of analog stick on x-axis
 
 		// When smash mode is activated, you walk instead of dash, and do smash attacks instead of tilts.
-		if (Input.GetKeyDown(KeyCode.A)) smashMode = true;
-		else if (Input.GetKeyUp(KeyCode.A)) smashMode = false;
+		if (Input.GetKeyDown(KeyCode.LeftShift)) isWalking = true;
+		else if (Input.GetKeyUp(KeyCode.LeftShift)) isWalking = false;
 
 		// Calls the Jump function when spacebar is pressed and you still have jumps left.
 		if ((Input.GetKeyDown(KeyCode.Space)) && (jumpsUsed < maxNbJumps)) {
-			if (isGrounded && !smashMode) Jump(0);      // Perform a standard jump.
-			else if (isGrounded && smashMode) Jump(1);  // Perform a short jump.
+			if (isGrounded && !isWalking) Jump(0);      // Perform a standard jump.
+			else if (isGrounded && isWalking) Jump(1);  // Perform a short jump.
 			else if (!isGrounded) Jump(2);              // Perform a double jump.
 		}
 
-		if (Input.GetKeyDown(KeyCode.C)) Physics2D.IgnoreLayerCollision(10, 9);  // Go through platforms.
+		if (Input.GetKeyDown(KeyCode.N) && !isGrounded) isAirDodging = true;
+
+		if (Input.GetKeyDown(KeyCode.X)) Physics2D.IgnoreLayerCollision(10, 9);  // Go through platforms.
 		else if (!insidePlateform) {
 			Physics2D.IgnoreLayerCollision(10, 9, false);  // Reactivate collisions when out of platform.
 		}
@@ -62,37 +73,75 @@ public class Character_script : MonoBehaviour {
 		if (isGrounded) {
 			jumpsUsed = 0;  // Resets the number of jumps performed when touching the ground.
 			isFastFalling = false; // Deactivates the fast-fall.
+			ableToAirDodge = true;
+			if (isAirDodging) {
+				isAirDodging = false;  // Cancles the air dodge when landing to allow for combos.
+				rb.gravityScale = gravity;  // Reactivate gravity.
+				rb.drag = normalDrag;  // Set the linear drag to the regular value.
+			}
 		}
 
-		if (horizontalMove != 0) {  // If the player is moving.
-			if (isGrounded) {  // If the player is on the ground.
-				if (smashMode) {  // Physics when walking.
-					rb.velocity = new Vector2(horizontalMove * walkSpeed, rb.velocity.y);
-				}
-				else {  // Physics when running.
-					if (ableToDash) {  // Performs a dash at the start of the run.
-						rb.AddForce (new Vector2(dashSpeed * horizontalMove, 0f), ForceMode2D.Impulse);
-						ableToDash = false;
-					}
-					else {  // will slow down naturaly due to the friction and keep the player at running speed.
-						float x =  Mathf.Clamp(Abs(rb.velocity.x), runSpeed, dashSpeed);
-						rb.velocity = new Vector2(x * horizontalMove, rb.velocity.y);
-					}
+		if (isAirDodging) {
+			if (ableToAirDodge) {
+				ableToAirDodge = false;
+				// Deactivate hurbox
+
+				if (Abs(horizontalMove) >= 0.1f || Abs(verticalMove) >= 0.1f) {
+					rb.velocity = new Vector2(0f, 0f);
+					dirAirDodge = true;
+					rb.gravityScale = 0f;  // Turn off gravity.
+					rb.drag = 0f;  // Change the value of the drag.
+					float angle = Abs(Mathf.Atan(verticalMove / horizontalMove));
+					rb.AddForce (new Vector2 (Mathf.Cos(angle) *horizontalMove *airDodgeDist,
+								Mathf.Sin(angle) *verticalMove *airDodgeDist), ForceMode2D.Impulse);
+					rb.drag = airDodgeDrag;
 				}
 			}
-			else {  // If the player is in the air.
-				// Slowly accelerate when in the air.
-				rb.AddForce (new Vector2(horizontalMove * airAccel, rb.velocity.y), ForceMode2D.Force);
-				// Speed will not go faster than the value airSpeed variable.
-				float x = Mathf.Clamp(rb.velocity.x, -airSpeed, airSpeed);
-				rb.velocity = new Vector2(x, rb.velocity.y);
+			
+			// Cancle everything when total velocity is too low.
+			float speed = Mathf.Sqrt(Mathf.Pow(rb.velocity.x, 2) + Mathf.Pow(rb.velocity.y, 2));
+			if (dirAirDodge && speed <= 0.3f) {
+				rb.gravityScale = gravity;
+				rb.drag = normalDrag;
+				isAirDodging = false;
+			}
+
+			else if (!dirAirDodge && rb.velocity.y <= -3f) {  // Replace velocity check with timer.
+				isAirDodging = false;
 			}
 		}
-		
-		if (isFastFalling) rb.velocity = new Vector2(rb.velocity.x, fastFallSpeed);  // Apply fast-fall speed.
-		else if (rb.velocity.y < 0) {  // Limits the maximum speed of the player when falling down.
-			float y =  Mathf.Clamp(rb.velocity.y, maxFallSpeed, 0f);
-			rb.velocity = new Vector2(rb.velocity.x, y);
+
+		else {
+			if (horizontalMove != 0) {  // If the player is moving.
+				if (isGrounded) {  // If the player is on the ground.
+					if (isWalking) {  // Physics when walking.
+						rb.velocity = new Vector2(horizontalMove * walkSpeed, rb.velocity.y);
+					}
+					else {  // Physics when running.
+						if (ableToDash) {  // Performs a dash at the start of the run.
+							rb.AddForce (new Vector2(dashSpeed * horizontalMove, 0f), ForceMode2D.Impulse);
+							ableToDash = false;
+						}
+						else {  // will slow down naturaly due to the friction and keep the player at running speed.
+							float x =  Mathf.Clamp(Abs(rb.velocity.x), runSpeed, dashSpeed);
+							rb.velocity = new Vector2(x * horizontalMove, rb.velocity.y);
+						}
+					}
+				}
+				else {  // If the player is in the air.
+					// Slowly accelerate when in the air.
+					rb.AddForce (new Vector2(horizontalMove * airAccel, rb.velocity.y), ForceMode2D.Force);
+					// Speed will not go faster than the value airSpeed variable.
+					float x = Mathf.Clamp(rb.velocity.x, -airSpeed, airSpeed);
+					rb.velocity = new Vector2(x, rb.velocity.y);
+				}
+			}
+			
+			if (isFastFalling) rb.velocity = new Vector2(rb.velocity.x, fastFallSpeed);  // Apply fast-fall speed.
+			else if (rb.velocity.y < 0) {  // Limits the maximum speed of the player when falling down.
+				float y =  Mathf.Clamp(rb.velocity.y, maxFallSpeed, 0f);
+				rb.velocity = new Vector2(rb.velocity.x, y);
+			}
 		}
 	}
 
